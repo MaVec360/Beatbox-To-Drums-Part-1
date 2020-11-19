3ecc0dc75587f4eb21253ef2981f46fb3386d7b1
Max Vecchitto <maxvecchitto@Maxs-MacBook-Pro.local>
2020-11-19 16:15:30 -0700

    Can't figure out where to put recorder function coding.

--- libraries/sketch.js
+++ libraries/sketch.js
@@ -1,4 +1,5 @@
-let fft, mic, songfile;
+let fft, mic, recorder, songFile;
+var state = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -43,3 +44,75 @@ vertex(x, y);
 }
 endShape();
 }
+
+
+
+///Can't figure out how to record audio. There is this js code to create a button and when you click it, it records audio.
+//But when I add the button code into the setup, it makes all the other functionality disappear.
+//where can I add this code to get it to record audio AND show the FTT analysis?
+
+//var mic, recorder, soundFile;
+//var state = 0;
+
+//function setup() {
+//  createCanvas(600,600);
+//  background(200);
+  // create an audio in
+  
+//	button = createButton('click me');
+//  button.position(19, 19);
+//  button.mousePressed(startRec);
+//  mic = new p5.AudioIn();
+
+  // prompts user to enable their browser mic
+
+
+//  mic.start();
+//  mic.start();
+
+  // create a sound recorder
+  //recorder = new p5.SoundRecorder();
+
+  // connect the mic to the recorder
+  //recorder.setInput(mic);
+
+  // this sound file will be used to
+  // playback & save the recording
+  //soundFile = new p5.SoundFile();
+
+ // text('keyPress to record', 20, 20);
+//}
+
+//function keyPressed() {
+  // make sure user enabled the mic
+
+//}
+
+//function startRec(){
+//  if (state === 0 && mic.enabled) {
+
+    // record to our p5.SoundFile
+//    recorder.record(soundFile);
+
+//    background(255,0,0);
+//    text('Recording!', 20, 20);
+//    state++;
+//  }
+//  else if (state === 1) {
+//    background(0,255,0);
+
+    // stop recorder and
+    // send result to soundFile
+//    recorder.stop();
+
+//    text('Stopped', 20, 20);
+//    state++;
+//  }
+
+//  else if (state === 2) {
+//    soundFile.play(); // play the result!
+//    save(soundFile, 'mySound.wav');
+//    state++;
+//  }
+
+//} 
